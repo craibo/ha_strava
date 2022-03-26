800da87053cf0b2e84c503901be64fbe6376b231
madmic1314 <53381142+madmic1314@users.noreply.github.com>
2022-03-26 19:57:54 +0000

    Update sensor.py

--- custom_components/ha_strava/sensor.py
+++ custom_components/ha_strava/sensor.py
@@ -331,12 +331,9 @@ class StravaStatsSensor(Entity):
             return distance
 
         if metric == CONF_SENSOR_PACE:
-            if self._data[CONF_SENSOR_DISTANCE] > 0:
-                pace = self._data[CONF_SENSOR_MOVING_TIME] / (
-                    self._data[CONF_SENSOR_DISTANCE] / 1000
-                )
-            else:
-                pace = 0
+            pace = self._data[CONF_SENSOR_MOVING_TIME] / (
+                self._data[CONF_SENSOR_DISTANCE] / 1000
+            )
             unit = f"{TIME_MINUTES}/{LENGTH_KILOMETERS}"
             if not self.hass.config.units.is_metric:
                 pace = (self._data[CONF_SENSOR_MOVING_TIME]) / (
