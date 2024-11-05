ae5c3b70260ff6a4eabcbc06f0f734c0b2374687
craibo <tim@swipejobs.com>
2024-11-05 10:50:33 +0000

    Handle missing await on async_forward_entry_setups

--- custom_components/ha_strava/__init__.py
+++ custom_components/ha_strava/__init__.py
@@ -869,7 +869,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     ]
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     )
 
     return True
